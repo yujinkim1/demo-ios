e74303c54393deccb4d20c102dea882e5547257d
yujinkim1 <yujin7621@me.com>
2023-01-25 22:41:38 +0900

    add: Object detect functions

--- ObjectDetection/ObjectDetection/DetectController.swift
+++ ObjectDetection/ObjectDetection/DetectController.swift
@@ -4,21 +4,30 @@
 import Foundation
 import AVFoundation
 import Vision
+import UIKit
 
-extension ViewController {
+class ObjectDetectRecognitionViewController {
     
-    func setupDetector() {
-        let mlmodel = Bundle.main.url(forResource: "yolov7safty", withExtension: "mlmodelc")
+    //MARK: VISION
+    private var requests = [VNRequest]()
+    private var detectionOverlay: CALayer! = nil
+    private var firstLabel: String = ""
+    private var firstConfidence: Float = 0.0
+    
+    @discardableResult
+    func setupDetector() -> NSError? {
         
+        guard let mlmodel = Bundle.main.url(forResource: "yolov7safty", withExtension: "mlmodelc") else {
+            return NSError(domain: "ObjectDetectRecognitionViewController", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
+        }
         do {
-            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: mlmodel!))
-            let recognitions = VNCoreMLRequest(model: visionModel, completionHandler: detectionDidComplete)
-            self.requests = [recognitions]
-        } catch let error {
-            print(error)
+            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: mlmodel))
+            let recognition = VNCoreMLRequest(model: visionModel, completionHandler: detectionDidComplete)
+            
+        } catch let error as NSError {
+            print("Model loading went wrong: \(error)")
         }
     }
-    
     
     func detectionDidComplete(request: VNRequest, error: Error?) {
         DispatchQueue.main.async {
@@ -28,18 +37,119 @@ extension ViewController {
         }
     }
     
+    func drawVisionRequestResults(_ results: [Any]) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        //Remove all the old recognized objects
+        detectionOverlay.sublayers = nil
+        
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard observation is VNRecognizedObjectObservation else {
+                continue
+            }
+            
+            //Select only the label with the highest confidence
+            let topLabelObservation = objectObservation.labels[0]
+            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
+           
+            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+            
+            let textLayer = self.createTextSubLayerInBounds(objectBounds,
+                                                            identifier: topLabelObservation.identifier,
+                                                            confidence: topLabelObservation.confidence)
+            shapeLayer.addSublayer(textLayer)
+            detectionOverlay.addSublayer(shapeLayer)
+        }
+        self.updateLayerGeometry()
+        CATransaction.commit()
+    }
+    
+    override func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        let exifOrientation = exifOrientationFromDeviceOrientation()
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: exifOrientation, options: [:])
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    override func setupAVCapture() {
+        super.setupAVCapture()
+        
+        // setup Vision parts
+        setupLayers()
+        updateLayerGeometry()
+        setupVision()
+        
+        // start the capture
+        startCaptureSession()
+    }
+    
     //MARK: BASIC LAYER
     func setupLayers() {
-        //...
+        detectionOverlay = CALayer() // container layer that has all the renderings of the observations
+        detectionOverlay.name = "DetectionOverlay"
+        detectionOverlay.bounds = CGRect(x: 0.0,
+                                         y: 0.0,
+                                         width: bufferSize.width,
+                                         height: bufferSize.height)
+        detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+        rootLayer.addSublayer(detectionOverlay)
     }
     
     //MARK: UPDATE BASIC LAYER
-    func updateLayers() {
-        //...
+    func updateLayerGeometry() {
+            let bounds = rootLayer.bounds
+            var scale: CGFloat
+            
+            let xScale: CGFloat = bounds.size.width / bufferSize.width
+            let yScale: CGFloat = bounds.size.height / bufferSize.height
+            
+            scale = fmax(xScale, yScale)
+            if scale.isInfinite {
+                scale = 1.0
+            }
+            CATransaction.begin()
+            CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+            // Rotate the layer into screen orientation and scale and mirror
+            // Change the quotient to 1.0 for portrait
+            detectionOverlay.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(0.0)).scaledBy(x: scale, y: -scale))
+            // Centre layer
+            detectionOverlay.position = CGPoint(x: bounds.midX, y: bounds.midY)
+            CATransaction.commit()
+        }
+    
+    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
+        let textLayer = CATextLayer()
+        textLayer.name = "Object Label"
+        let formattedString = NSMutableAttributedString(string: String(format: "\(identifier)\nConfidence:  %.2f", confidence))
+        let largeFont = UIFont(name: "Helvetica", size: 24.0)!
+        formattedString.addAttributes([NSAttributedString.Key.font: largeFont], range: NSRange(location: 0, length: identifier.count))
+        textLayer.string = formattedString
+        textLayer.bounds = CGRect(x: 0, y: 0, width: bounds.size.height - 10, height: bounds.size.width - 10)
+        textLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        textLayer.shadowOpacity = 0.7
+        textLayer.shadowOffset = CGSize(width: 2, height: 2)
+        textLayer.foregroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.0, 0.0, 0.0, 1.0])
+        textLayer.contentsScale = 2.0 // retina rendering
+        // rotate the layer into screen orientation and scale and mirror
+        textLayer.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: 1.0, y: -1.0))
+        return textLayer
     }
     
-    //MARK: DETECT OBJECTS WITH SHOW BOX
-    func drawBox() {
-        //...
+    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.name = "Found Object"
+        shapeLayer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 0.2, 0.4])
+        shapeLayer.cornerRadius = 7
+        return shapeLayer
     }
 }
