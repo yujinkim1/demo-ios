aa841a2622c782ba02030f30566ae5c8117856b2
yujinkim1 <yujin7621@me.com>
2023-03-11 23:27:55 +0900

    modi: Specify parent comments

--- ObjectDetection/ObjectDetection/ViewController.swift
+++ ObjectDetection/ObjectDetection/ViewController.swift
@@ -1,19 +1,27 @@
 //  ViewController.swift
-//  Created by yujinkim on 2023/01/18.
 
 import SwiftUI
 import UIKit
 import AVFoundation
 import Vision
 
-class ViewController: UIViewController {
+/**
+ * 1. AVCaptureSession을 사용
+ * 2. Preview Layer를 지정
+ * 3. 객체를 탐지하고, 이를 바운딩 박스를 통해 아웃풋을 표현
+ * 3. 이와 관련된 View Controller를 Root Layer에 연결 (메인 쓰레드와 연관이 있다고 봄)
+ */
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     private var permissionGranted = false
     private let captureSession = AVCaptureSession()
+    //AVCaptureSession을 사용하기 위한 디스패치 큐
     private let sessionQueue = DispatchQueue(label: "sessionQueue")
     private var previewLayer = AVCaptureVideoPreviewLayer()
     var screenRect: CGRect! = nil
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         checkPermission()
         
         sessionQueue.async { [unowned self] in
@@ -23,6 +31,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //MARK: 디바이스의 기본 방향에 대한 케이스 함수
     override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
         screenRect = UIScreen.main.bounds
         self.previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
@@ -49,7 +58,7 @@ class ViewController: UIViewController {
                 break
         }
     }
-    
+    //MARK: 사용 권한 확인 함수
     func checkPermission() {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
             case .authorized:
@@ -60,7 +69,7 @@ class ViewController: UIViewController {
                 permissionGranted = false
         }
     }
-    
+    //MARK: Not determind 시 권한 요청을 하기 위한 함수
     func requestPermission() {
         sessionQueue.suspend()
         AVCaptureDevice.requestAccess(for: .video) { [unowned self] granted in self.permissionGranted = granted
@@ -68,10 +77,13 @@ class ViewController: UIViewController {
         }
     }
     
-    //MARK: ACCESS CAMERA
+    //MARK: 카메라 허용 및 셋업 함수
     func setupCaptureSession() {
         guard let videoDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) else { return }
         guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
+        
+        //session?.beginConfiguration()
+        //session?.sessionPreset = .vga640x480
         
         guard captureSession.canAddInput(videoDeviceInput) else { return }
         captureSession.addInput(videoDeviceInput)
